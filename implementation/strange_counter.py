47a118b9965a78462918df16dbab5fcd487bc0a2
angelgladin <angelgladin@gmail.com>
2018-05-02 13:17:38 -0500

    :squirrel:

---
+++ implementation/strange_counter.py
@@ -0,0 +1,17 @@
+n = int(input())
+max_score = 10e12
+
+a, b = 1, 3
+t_v_l = []
+r = 0
+
+while b < max_score:
+    t_v_l.append((a, b,))
+    a += b
+    b *= 2
+
+for i in range(len(t_v_l)):
+    if(t_v_l[i][0] <= n):
+        r = t_v_l[i][1] - (n - t_v_l[i][0])
+
+print(r)
